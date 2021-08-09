a43f021e46a0630c7b387378d9b34ff73959c8f7
Sara <sluisdejesus@gmail.com>
2021-08-09 15:12:54 +0100

    modified seeds.js

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,45 +1,15 @@
-use carbonData;
-
+use users;
 db.dropDatabase();
 
-db.transport.insertMany([
+db.userInfo.insertMany([
     {
-        "transport":"Train",
-        "co2_grams_per_km":39.94
+      "Name":"Jeremy Clarkson",
+      "Diet":"Omnivore",
+      "Transport":"Car"
     },
     {
-        "transport":"E car",
-        "co2_grams_per_km":57.28
-    },
-    {
-        "transport":"D car",
-        "co2_grams_per_km":168.44
-    },
-    {
-        "transport":"P car",
-        "co2_grams_per_km":174.3
-    },
-    {
-        "transport":"Coach",
-        "co2_grams_per_km":27.32
-    },
-    {
-        "transport":"Motorbike",
-        "co2_grams_per_km":113.37
-    }
-])
-
-db.diet.insertMany([
-    {
-        "diet":"Vegan",
-        "co2_grams_per_day":2890
-    },
-    {
-        "diet":"Vegetarian",
-        "co2_grams_per_day":3810
-    },
-    {
-        "diet":"Omnivore",
-        "co2_grams_per_day":5630
+        "Name":"Moby",
+        "Diet":"Vegan",
+        "Transport":"Bike"
     }
 ])
