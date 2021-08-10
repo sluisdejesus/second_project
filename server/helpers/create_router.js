96f10bef0af8fceb06d2c644277e4a77d150e40c
Sergio <sergio.eneide@protonmail.com>
2021-08-10 12:13:20 +0100

    created get route

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -1,20 +1,25 @@
 const express = require('express');
-const ObjectId = require('mongodb').ObjectId;
+const ObjectID = require('mongodb').ObjectID;
 
 const createRouter = function(collection) {
-    const router = express.Router();
 
-    router.get('/', (req,res) => {
-        collection
-        .find()
-        .toArray()
-        .then((docs) => res.json(docs))
-        .catch((err) => {
-            console.error(err);
-            res.status(500);
-            res.json({status:500, error:err })
-        });
+  const router = express.Router();
+
+  router.get('/', (req, res) => {
+    collection
+    .find()
+    .toArray()
+    .then((docs) => res.json(docs))
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
     });
+  });
 
-    
-}
+
+
+  return router;
+};
+
+module.exports = createRouter;
