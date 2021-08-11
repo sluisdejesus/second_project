cb75ddcbf38423d48a0e1eac84ca42a8790be4f5
Stephanie <stephania.l.e@gmail.com>
2021-08-11 11:30:57 +0100

    added add on carbonServices.js

--- client/src/services/carbonServices.js
+++ client/src/services/carbonServices.js
@@ -1,8 +1,21 @@
 const baseURL = 'http://localhost:5000/api/carbon/';
 
 const carbonServices = {
-    getServices() {
+    getCarbonData() {
         return fetch(baseURL)
         .then(res => res.json());
+    },
+
+    addCarbonData(data) {
+        return fetch(baseURL, {
+            method:'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json());
+
     }
+    
 }
